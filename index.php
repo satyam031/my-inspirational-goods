d7d48f2aa4b7693f0038647021977d780872269f
SATYAM <sk99129655@gmail.com>
2021-02-18 10:27:15 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    This is my first website
+
+    <?php
+        echo "hello world happy to see you here";
+    ?>
+    </div>
+</body>
+</html>
